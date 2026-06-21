ef095b9abae7953686757722bb2e34d15cff45b0
zakariaw <zwardere1@gmail.com>
2026-06-21 20:19:56 +1000

    reactnative reuasable with uniwnd installed

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -1,36 +1,41 @@
+import "@/global.css";
 import { Ionicons } from "@expo/vector-icons";
+import { PortalHost } from "@rn-primitives/portal";
 import { Tabs } from "expo-router";
 
 export default function RootLayout() {
   return (
-    <Tabs screenOptions={{ tabBarActiveTintColor: "blue" }}>
-      <Tabs.Screen
-        name="index"
-        options={{
-          title: "Home",
-          tabBarIcon: ({ color }) => (
-            <Ionicons name="home" size={20} color={color} />
-          ),
-        }}
-      />
-      <Tabs.Screen
-        name="vocab"
-        options={{
-          title: "Vocab",
-          tabBarIcon: ({ color }) => (
-            <Ionicons name="book" size={20} color={color} />
-          ),
-        }}
-      />
-      <Tabs.Screen
-        name="settings"
-        options={{
-          title: "Settings",
-          tabBarIcon: ({ color }) => (
-            <Ionicons name="person" size={20} color={color} />
-          ),
-        }}
-      />
-    </Tabs>
+    <>
+      <Tabs screenOptions={{ tabBarActiveTintColor: "blue" }}>
+        <Tabs.Screen
+          name="index"
+          options={{
+            title: "Home",
+            tabBarIcon: ({ color }) => (
+              <Ionicons name="home" size={20} color={color} />
+            ),
+          }}
+        />
+        <Tabs.Screen
+          name="vocab"
+          options={{
+            title: "Vocab",
+            tabBarIcon: ({ color }) => (
+              <Ionicons name="book" size={20} color={color} />
+            ),
+          }}
+        />
+        <Tabs.Screen
+          name="settings"
+          options={{
+            title: "Settings",
+            tabBarIcon: ({ color }) => (
+              <Ionicons name="person" size={20} color={color} />
+            ),
+          }}
+        />
+      </Tabs>
+      <PortalHost />
+    </>
   );
 }
